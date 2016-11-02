3eaab9a043cfae530e5470d43b5eb31960dfd3af
Caitlin Flattery <caitlinflattery@gmail.com>
2016-11-02 15:29:36 -0500

    improved styling

--- src/scripts/SingleView.js
+++ src/scripts/SingleView.js
@@ -4,17 +4,15 @@ import Header from './header'
 import Backbone from 'backbone'
 
 var SingleView = React.createClass({
-
-
     render: function () {
-        // console.log(this.props)
         return (
             <div className = 'singleView'>
                 <Header />
                 <div className = 'singleItemContainer'>
                     <div className = 'singleTitle'>{this.props.itemModel.get('results')[0].title}</div>
                     <img src = {this.props.itemModel.get('results')[0].Images[0].url_570xN} />
-                    <div className = 'singlePrice'>{this.props.itemModel.get('results')[0].price}</div>
+                    <div className = 'singlePrice'>${this.props.itemModel.get('results')[0].price}</div>
+                    <div className = 'singleDescription'>{this.props.itemModel.get('results')[0].description}</div>
                 </div>
             </div>
         )
